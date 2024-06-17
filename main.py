363c08eaf6e500cb7cf2dbd4e9c5a4fc5ad98471
senolita-git <snaymnn@gmail.com>
2024-06-17 15:05:53 +0200

    user deletion feature

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user, post, comment,status, groups, posts, request,chat
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -21,6 +22,20 @@ app.include_router(request.router)
 @app.get("/")
 def root():
     return "Hello world!"
+
+origins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
 
 models.Base.metadata.create_all(engine)
 
